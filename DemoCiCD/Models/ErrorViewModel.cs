edf2084a18a1c12ceda43c2d979ed1bf4604d929
Tran Trung <trantrung28122003@gmail.com>
2024-04-05 09:16:29 +0700

    init file

---
+++ DemoCiCD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoCiCD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
